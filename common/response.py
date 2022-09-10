18c6dc001f1ec6b59a307ccc89039b2be53b9b58
Sunil Goyal <admin@rimiktech.com>
2022-09-10 14:33:52 +0530

    added response class

---
+++ common/response.py
@@ -0,0 +1,22 @@
+from flask import Response
+import json
+from decimal import Decimal
+import datetime
+
+class response:
+
+    class DecimalEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, Decimal):
+                return str(obj)
+            if isinstance(obj, datetime.datetime):
+                return obj.__str__()
+            return json.JSONEncoder.default(self, obj)
+
+    def __init__(self, status=200, message="", data=None):
+        result = { "status": status, "message": message, "data": data }
+        self.body = json.dumps(result, cls=response.DecimalEncoder)
+
+    def __str__(self): return self.body
+
+    def get_json(self): return Response(self.body, mimetype="application/json")
